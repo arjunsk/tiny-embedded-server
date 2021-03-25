3ec2b99e2b00a5896779ef7299213ec09e41dac7
Arjun SK <arjun.sunilkumar@nissanmotor.com>
2021-03-26 01:02:49 +0530

    Added Socket Loop

--- java-net-examples/socket/src/main/java/com/arjunsk/eg/socket/Server.java
+++ java-net-examples/socket/src/main/java/com/arjunsk/eg/socket/Server.java
@@ -1,21 +1,30 @@
 package com.arjunsk.eg.socket;
 
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+@SuppressWarnings("java:S2189")
 public class Server {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+
+    DataInputStream inputStream = null;
+
     try (ServerSocket serverSocket = new ServerSocket(6666)) {
-      Socket socket = serverSocket.accept(); // establishes connection
 
-      DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-      String str = inputStream.readUTF();
-      System.out.println("Message = " + str);
+      while (true) {
+        Socket socket = serverSocket.accept(); // establishes connection
 
-      inputStream.close();
+        inputStream = new DataInputStream(socket.getInputStream());
+        String str = inputStream.readUTF();
+        System.out.println("Message = " + str);
+      }
+
     } catch (Exception ex) {
       ex.printStackTrace();
+    } finally {
+      inputStream.close();
     }
   }
 }
